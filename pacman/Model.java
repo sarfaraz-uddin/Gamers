947e7167e61de9a4ecf15d63632b86f9e4a238bb
sarfaraz-uddin <88956341+sarfaraz-uddin@users.noreply.github.com>
2023-05-08 19:57:50 +0545

    started making maze

---
+++ pacman/Model.java
@@ -0,0 +1,65 @@
+package pacman;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Model extends JPanel {
+
+	private Dimension d;
+    private final Font smallFont = new Font("Arial", Font.BOLD, 14);
+    private boolean inGame = false;
+    private boolean dying = false;
+
+    private final int BLOCK_SIZE = 24;
+    private final int N_BLOCKS = 15;
+    private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
+    private final int MAX_GHOSTS = 12;
+    private final int PACMAN_SPEED = 6;
+
+    private int N_GHOSTS = 6;
+    private int lives, score;
+    private int[] dx, dy;
+    private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
+
+    private Image heart, ghost;
+    private Image up, down, left, right;
+
+    private int pacman_x, pacman_y, pacmand_x, pacmand_y;
+    private int req_dx, req_dy;
+
+    private final short levelData[] = {
+    	19, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 22,
+        17, 16, 16, 16, 16, 24, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        25, 24, 24, 24, 28, 0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
+        0,  0,  0,  0,  0,  0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
+        19, 18, 18, 18, 18, 18, 16, 16, 16, 16, 24, 24, 24, 24, 20,
+        17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
+        17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
+        17, 16, 16, 16, 24, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
+        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 18, 18, 18, 18, 20,
+        17, 24, 24, 28, 0, 25, 24, 24, 16, 16, 16, 16, 16, 16, 20,
+        21, 0,  0,  0,  0,  0,  0,   0, 17, 16, 16, 16, 16, 16, 20,
+        17, 18, 18, 22, 0, 19, 18, 18, 16, 16, 16, 16, 16, 16, 20,
+        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
+        25, 24, 24, 24, 26, 24, 24, 24, 24, 24, 24, 24, 24, 24, 28
+    };
+
+    private final int validSpeeds[] = {1, 2, 3, 4, 6, 8};
+    private final int maxSpeed = 6;
+
+    private int currentSpeed = 3;
+    private short[] screenData;
+    private Timer timer;
+
+
+    
+    
+
+	}
