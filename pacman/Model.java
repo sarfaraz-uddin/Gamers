e5639cff0bc2d2053ed05e2baae40f287ae98f3f
anjanshrestha8 <anjanshrestha2023@gmail.com>
2023-05-09 18:59:14 +0545

    final work done

--- pacman/Model.java
+++ pacman/Model.java
@@ -9,7 +9,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Model extends JPanel {
+public class Model extends JPanel implements ActionListener{
 
 	private Dimension d;
     private final Font smallFont = new Font("Arial", Font.BOLD, 14);
@@ -27,7 +27,7 @@ public class Model extends JPanel {
     private int[] dx, dy;
     private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
 
-    private Image heart, ghost;
+    private Image heart, ghost,pacman;
     private Image up, down, left, right;
 
     private int pacman_x, pacman_y, pacmand_x, pacmand_y;
@@ -62,18 +62,18 @@ public class Model extends JPanel {
 
         loadImages();
         initVariables();
-//        addKeyListener(new TAdapter());
+        addKeyListener(new TAdapter());
         setFocusable(true);
-//        initGame();
+        initGame();
     }
     private void loadImages() {
-        down = new ImageIcon("C:\\Users\\Dell\\IdeaProjects\\Pacman\\images\\down.gif").getImage();
-        up = new ImageIcon("C:\\Users\\Dell\\IdeaProjects\\Pacman\\images\\up.gif").getImage();
-        left = new ImageIcon("C:\\Users\\Dell\\IdeaProjects\\Pacman\\images\\left.gif").getImage();
-        right = new ImageIcon("C:\\Users\\Dell\\IdeaProjects\\Pacman\\images\\right.gif").getImage();
-        ghost = new ImageIcon("C:\\Users\\Dell\\IdeaProjects\\Pacman\\images\\ghost.gif").getImage();
-        heart = new ImageIcon("C:\\Users\\Dell\\IdeaProjects\\Pacman\\images\\heart.png").getImage();
-
+        down = new ImageIcon("/Users/anjanshrestha/Downloads/GitHub/Gamers/images/down.gif").getImage();
+        ghost = new ImageIcon("/Users/anjanshrestha/Downloads/GitHub/Gamers/images/ghost.gif").getImage();
+        heart = new ImageIcon("/Users/anjanshrestha/Downloads/GitHub/Gamers/images/heart.png").getImage();
+        left = new ImageIcon("/Users/anjanshrestha/Downloads/GitHub/Gamers/images/left.gif").getImage();
+        pacman = new ImageIcon("/Users/anjanshrestha/Downloads/GitHub/Gamers/images/pacman.png").getImage();
+        right = new ImageIcon("/Users/anjanshrestha/Downloads/GitHub/Gamers/images/right.gif").getImage();
+        up= new ImageIcon("/Users/anjanshrestha/Downloads/GitHub/Gamers/images/up.gif").getImage();
     }
     private void initVariables() {
 
@@ -87,8 +87,8 @@ public class Model extends JPanel {
         dx = new int[4];
         dy = new int[4];
 
-//        timer = new Timer(40, this);
-//        timer.start();
+        timer = new Timer(40, this);
+        timer.start();
     }
 
     private void playGame(Graphics2D g2d) {
@@ -150,7 +150,7 @@ public class Model extends JPanel {
                 currentSpeed++;
             }
 
-//            initLevel();
+            initLevel();
         }
     }
 
@@ -165,7 +165,7 @@ public class Model extends JPanel {
             g1.drawString(start, (SCREEN_SIZE)/2, 200);
         }
 
-//        continueLevel();
+        continueLevel();
     }
 
     private void moveGhosts(Graphics2D g2d) {
@@ -293,7 +293,161 @@ public class Model extends JPanel {
             g2d.drawImage(down, pacman_x + 1, pacman_y + 1, this);
         }
     }
-    
-    
+    private void drawMaze(Graphics2D g2d) {
 
-	}
+        short i = 0;
+        int x, y;
+
+        for (y = 0; y < SCREEN_SIZE; y += BLOCK_SIZE) {
+            for (x = 0; x < SCREEN_SIZE; x += BLOCK_SIZE) {
+
+                g2d.setColor(new Color(0,72,251));
+                g2d.setStroke(new BasicStroke(5));
+
+                if ((levelData[i] == 0)) {
+                    g2d.fillRect(x, y, BLOCK_SIZE, BLOCK_SIZE);
+                }
+
+                if ((screenData[i] & 1) != 0) {
+                    g2d.drawLine(x, y, x, y + BLOCK_SIZE - 1);
+                }
+
+                if ((screenData[i] & 2) != 0) {
+                    g2d.drawLine(x, y, x + BLOCK_SIZE - 1, y);
+                }
+
+                if ((screenData[i] & 4) != 0) {
+                    g2d.drawLine(x + BLOCK_SIZE - 1, y, x + BLOCK_SIZE - 1,
+                            y + BLOCK_SIZE - 1);
+                }
+
+                if ((screenData[i] & 8) != 0) {
+                    g2d.drawLine(x, y + BLOCK_SIZE - 1, x + BLOCK_SIZE - 1,
+                            y + BLOCK_SIZE - 1);
+                }
+
+                if ((screenData[i] & 16) != 0) {
+                    g2d.setColor(new Color(255,255,255));
+                    g2d.fillOval(x + 10, y + 10, 6, 6);
+                }
+
+                i++;
+            }
+        }
+    }
+
+    private void initGame() {
+
+        lives = 3;
+        score = 0;
+        initLevel();
+        N_GHOSTS = 6;
+        currentSpeed = 3;
+    }
+
+    private void initLevel() {
+
+        int i;
+        for (i = 0; i < N_BLOCKS * N_BLOCKS; i++) {
+            screenData[i] = levelData[i];
+        }
+
+        continueLevel();
+    }
+
+    private void continueLevel() {
+
+        int dx = 1;
+        int random;
+
+        for (int i = 0; i < N_GHOSTS; i++) {
+
+            ghost_y[i] = 4 * BLOCK_SIZE; //start position
+            ghost_x[i] = 4 * BLOCK_SIZE;
+            ghost_dy[i] = 0;
+            ghost_dx[i] = dx;
+            dx = -dx;
+            random = (int) (Math.random() * (currentSpeed + 1));
+
+            if (random > currentSpeed) {
+                random = currentSpeed;
+            }
+
+            ghostSpeed[i] = validSpeeds[random];
+        }
+
+        pacman_x = 7 * BLOCK_SIZE;  //start position
+        pacman_y = 11 * BLOCK_SIZE;
+        pacmand_x = 0;	//reset direction move
+        pacmand_y = 0;
+        req_dx = 0;		// reset direction controls
+        req_dy = 0;
+        dying = false;
+    }
+
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setColor(Color.black);
+        g2d.fillRect(0, 0, d.width, d.height);
+
+        drawMaze(g2d);
+        drawScore(g2d);
+
+        if (inGame) {
+            playGame(g2d);
+        } else {
+            showIntroScreen(g2d);
+        }
+
+        Toolkit.getDefaultToolkit().sync();
+        g2d.dispose();
+    }
+
+
+//    controls
+    class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if (inGame) {
+                if (key == KeyEvent.VK_LEFT) {
+                    req_dx = -1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_RIGHT) {
+                    req_dx = 1;
+                    req_dy = 0;
+                } else if (key == KeyEvent.VK_UP) {
+                    req_dx = 0;
+                    req_dy = -1;
+                } else if (key == KeyEvent.VK_DOWN) {
+                    req_dx = 0;
+                    req_dy = 1;
+                } else if (key == KeyEvent.VK_ESCAPE && timer.isRunning()) {
+                    inGame = false;
+                }
+            } else {
+                if (key == KeyEvent.VK_SPACE) {
+                    inGame = true;
+                    initGame();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
+
+
+
+}
